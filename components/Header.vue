fdd5ff9142b99999de67cd989aa9e33915fea951
Iroha71 <y.koba.h1071@outlook.jp>
2019-11-30 13:23:53 +0900

    ➕ ヘッダー

---
+++ components/Header.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+<b-navbar type="dark" variant="primary">
+    <b-navbar-brand>
+        <img src="/images/camplus.png" />
+    </b-navbar-brand>
+    <b-collapse is-nav>
+        <b-navbar-nav>
+            <b-nav-item>参加者の傾向</b-nav-item>
+            <b-nav-item>おすすめ学科提案結果</b-nav-item>
+        </b-navbar-nav>
+        <button class="btn btn-danger"><img src="/images/logout.png">ログアウト</button>
+    </b-collapse>
+</b-navbar>
+</template>
+
+<style lang="scss" scoped>
+img{
+    height: 10vh;
+}
+.nav-link{
+    color: #f5f5f5 !important;
+}
+.btn{
+    position: absolute;
+    right: 0;
+    margin-right: 10px;
+    img{
+        height: 1.2rem;
+    }
+}
+</style>
---
+++ layouts/manage.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+    <div>
+        <Header class="header" />
+        <div class="container-fluid">
+            <nuxt />
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+export default {
+    components: {
+        Header
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.header{
+    height: 10vh;
+}
+.container-fluid{
+    height: 90vh;
+}
+</style>
