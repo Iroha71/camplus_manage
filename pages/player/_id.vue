5ceb876ee69099b3df273e4068a5226e337a01ba
Iroha71 <y.koba.h1071@outlook.jp>
2019-12-12 12:41:37 +0900

    🔼 ajax処理再実装

--- pages/player/_id.vue
+++ pages/player/_id.vue
@@ -1,11 +1,30 @@
 <template lang="html">
 <div class="row">
-    <h2 class="recommend">○○さんのおすすめ学科は<span>電子システム</span>です!!</h2>
-    
+    <h2 class="recommend">{{ player.name }}さんのおすすめ学科は<span :class="getFieldColor()">{{ player.field.name }}</span>です!!</h2>
 </div>
 </template>
 
-
+<script> 
+export default { 
+    async asyncData({store, route}) { 
+        const player_id = await route.params.id 
+        const player = await store.dispatch('api/req', {method: 'get', endpoint: `v1/player/${player_id}`, params: null})
+        return { player: player.data } 
+    },
+    methods: {
+        getFieldColor:function(){
+            switch(this.player.field.id){
+                case 1:
+                    return 'color-system'
+                case 2:
+                    return 'color-network'
+                case 3:
+                    return 'color-embeded'
+            }
+        }
+    }
+} 
+</script> 
 
 <style lang="scss" scoped>
 .row{
@@ -17,5 +36,17 @@
 .recommend{
     margin:auto;
     font-family: 'light-novel'
+}
+
+.color{
+    &-system{
+        color: #5757FF;
+    }
+    &-network{
+        color: #008500;
+    }
+    &-embeded{
+        color: #fcac01;
+    }
 }
 </style>
